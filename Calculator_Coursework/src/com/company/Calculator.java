3870c321999fbde842edc01183a4f538f2a5c33c
Lilit <39703980+LilitTakiryan@users.noreply.github.com>
2022-06-25 01:10:31 +0400

    Update Calculator.java

--- Calculator_Coursework/src/com/company/Calculator.java
+++ Calculator_Coursework/src/com/company/Calculator.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Calculator extends JFrame implements ActionListener/*, MenuListener*/ {
+public class Calculator extends JFrame implements ActionListener {
     Scientific sciObj;
     JMenu menu;
     JMenu help;
